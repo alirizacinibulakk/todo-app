d253e771920fb1f12a2db8ebd553fbdebd3a5472
Ali Rıza Çinibulak <alirizacinibulak@gmail.com>
2025-01-07 13:10:15 +0300

    csste düzenlemeler yapıldı

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [todos , setTodos] = useState(['Todo1','Todo2'])
+  const [todos , setTodos] = useState([])
   const [input, setInput] = useState("");
 
   function handleInputChange(e){
@@ -10,28 +10,59 @@ function App() {
   }
 
   function addTodo(){
-    console.log(input);
-    setTodos([...todos, input])
-    
+    setTodos([...todos, {
+      id: crypto.randomUUID(),
+      title: input,
+      completed: false,
+    }])
+    setInput("");
+    console.log(todos);
+  }
+  function handleCheckboxChange(e, todo){
+    todo.completed = !todo.completed;
+    setTodos([...todos]);
+  }
+
+  function handleDeleteTodo(id){
+    setTodos(todos.filter(x => x.id !== id));
+  }
+  function handleEditTodo(id){
+    setTodos(todos.map(x => {
+      if(x.id === id){
+        x.title = input;
+      }
+      return x;
+    }))
+    setInput("");
   }
   return (
-    <div className='container'>
+    <>
       <header>
         <h1>Todo App</h1>
       </header>
-      <div className="todo-add">
-        <input type="text" placeholder='Todo ekleyin' value={input}  onChange={handleInputChange}/>
-        <button onClick={addTodo}>Ekle</button>
+      <div className='container'>
+        <div className="todo-add">
+          <input type="text" placeholder='Todo ekleyin' value={input}  onChange={handleInputChange}/>
+          <button disabled={input.trim()== ""} onClick={addTodo}>Ekle</button>
+        </div>
+        <div className='todo-list-container'>
+            <ul className='todo-list'>
+              {
+                todos.map(todo => (
+                <li key={todo.id} className="todo">
+                  <label htmlFor={`todo-${todo.id}`}>
+                    <input id={`todo-${todo.id}`} type="checkbox" onChange={(e) => handleCheckboxChange(e,todo)}/>
+                  </label>
+                  <span className={`${todo.completed ? "line-through" : ""}`}>{todo.title}</span>
+                  <button onClick={() => handleDeleteTodo(todo.id)}>Sil</button>
+                  <button onClick={() => handleEditTodo(todo.id)}>Düzenle</button>
+                </li>                  
+                ))
+              }
+            </ul>
+          </div>
       </div>
-      <div>
-        {todos.map(todo => (
-          <>
-          <input type="checkbox" name="" id=""/>
-          <span key={todo}>{todo}</span>
-          </>
-        ))}
-      </div>
-    </div>
+    </>
   )
 }
 
